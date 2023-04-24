3648280cff178e2450c95069a18de308251e1ac6
softshaile-mf <shailendra.thakur@microfocus.com>
2023-04-24 23:09:02 +0100

    Static Website - S3 bucket creation with bucket policy

---
+++ tech-test/policy.tf
@@ -0,0 +1,15 @@
+data "aws_iam_policy_document" "static-site-policy" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    principals {
+      identifiers = ["*"]
+      type = "AWS"
+    }
+    resources = [
+      aws_s3_bucket.static-site-bucket.arn,
+       "${aws_s3_bucket.static-site-bucket.arn}/*",
+    ]
+  }
+}
---
+++ tech-test/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 static site domain name"
+  type        = string
+  default     = "s3-static-app-240042023"
+}
