84bea4549c15c8c2812f78b392335f42425ceef3
softshaile-mf <shailendra.thakur@microfocus.com>
2023-04-25 01:04:32 +0100

    Static Website - Released with Cloudfront Distribution

--- main.tf
+++ main.tf
@@ -154,3 +154,49 @@ resource "aws_iam_group_policy" "hr-group-policy" {
   group = aws_iam_group.hr-group.name
   policy = data.aws_iam_policy_document.hr.json
 }
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.static-site-bucket.bucket_regional_domain_name
+    origin_id   = var.bucket_name
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  custom_error_response {
+    error_code = "404"
+    response_page_path = "/error.html"
+    response_code = "404"
+  }
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.bucket_name
+    
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
