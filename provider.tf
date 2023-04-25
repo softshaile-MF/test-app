99057616caed826f966b2ca4072914e99c7c331f
softshaile-mf <shailendra.thakur@microfocus.com>
2023-04-25 03:10:57 +0100

    Static Website - Released with CI/CD usng github actions

--- provider.tf
+++ provider.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 4.64.0"
     }
   }
+  backend "s3" {
+    bucket         = "staticwebsite-terraform"
+    key            = "test/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
 
   required_version = ">= 1.3.0"
 }
