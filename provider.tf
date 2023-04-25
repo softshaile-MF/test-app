9487ad03d4331e2ded064ed633a80154b46c4590
softshaile-mf <shailendra.thakur@microfocus.com>
2023-04-25 03:29:27 +0100

    Static Website - Released with CI/CD usng github actions

--- provider.tf
+++ provider.tf
@@ -12,6 +12,7 @@ terraform {
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
+    profile        = "aws-tf-profile"
   }
 
 
@@ -19,6 +20,6 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.region
-  profile = "aws-terraform-profile"
+  region  = "us-east-1"
+  profile = "aws-tf-profile"
 }
