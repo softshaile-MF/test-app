58488be757f54a3e14f5ac29b4e4d308aa263f9f
softshaile-mf <shailendra.thakur@microfocus.com>
2023-04-26 11:44:58 +0100

    Static Website - Released with CI/CD usng github actions

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+bucket_name = "s3-static-app-250042023"
+region= "us-east-1"
+marketing-users=["Alice", "Malory"]
--- variables.tf
+++ variables.tf
@@ -1,20 +1,12 @@
 variable "region" {
   type    = string
-  default = "us-east-1"
 }
 
 variable "bucket_name" {
   description = "S3 static site domain name"
   type        = string
-  default     = "s3-static-app-250042023"
 }
 
 variable "marketing-users" {
   type    = list(string)
-  default = ["Alice", "Malory"]
-}
-variable "terraform-state" {
-  description = "terraform s3 state"
-  type        = string
-  default     = "staticwebsite-terraform"
 }
